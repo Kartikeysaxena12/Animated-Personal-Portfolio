0c0e4370b45624e1e39e90eefee11e3429b9a1d5
KARTIKEYA SAXENA <kartikeyasaxena99@gmail.com>
2024-08-29 13:54:24 +0530

    updated the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 //import logo from "../assets/kartik_logo.webp";
 //import log from "../assets/k_logo.png";
 import logos from "../assets/kevinRushLogo.png";
@@ -24,6 +24,72 @@ const Navbar = () => {
           <FaTwitterSquare />
           <FaInstagramSquare />
           <FaYoutube />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+*/
+import React from "react";
+import logos from "../assets/kevinRushLogo.png";
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+  FaYoutube,
+} from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="mb-8 flex items-center justify-between py-4">
+        <div className="flex flex-shrink-0 items-center lg:items-start">
+          <img className="w-12 text-neutral-400" src={logos} alt="logo" />
+        </div>
+        <div className="m-20 flex items-center justify-center gap-4 text-2xl ">
+          <a
+            href="http://www.linkedin.com/in/kartikeya-saxena-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-400 hover:text-white"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/Kartikeysaxena12/Animated-Personal-Portfolio"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-400 hover:text-white"
+          >
+            <FaGithubSquare />
+          </a>
+          <a
+            href="https://twitter.com/KartikeyaS75363"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-400 hover:text-white"
+          >
+            <FaTwitterSquare />
+          </a>
+          <a
+            href="https://www.instagram.com/kartikkofficialll/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-400 hover:text-white"
+          >
+            <FaInstagramSquare />
+          </a>
+          <a
+            href="https://www.youtube.com/@KartikeyaSaxena14"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-neutral-400 hover:text-white"
+          >
+            <FaYoutube />
+          </a>
         </div>
       </nav>
     </>
